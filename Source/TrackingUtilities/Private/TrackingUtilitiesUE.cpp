128cb8935a1a7eded5a2e8f1726318d51869d80e
zeplin455 <jdj.hans@gmail.com>
2021-10-10 21:31:03 +0200

    Initial push

---
+++ Source/TrackingUtilities/Private/TrackingUtilitiesUE.cpp
@@ -0,0 +1,22 @@
+
+#include "TrackingUtilitiesUE.h"
+#include "Modules/ModuleManager.h"
+
+
+#define LOCTEXT_NAMESPACE "FTRACKINGUTILITIESModule"
+
+
+void FTRACKINGUTILITIESModule::StartupModule()
+{
+
+
+}
+
+void FTRACKINGUTILITIESModule::ShutdownModule()
+{
+
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FTRACKINGUTILITIESModule, TrackingUtilities)
